3a697fd0cc86bcd229ffaecf4c37d19c844e03c0
root <root@raspberrypi.(none)>
2016-03-27 15:51:35 +0800

    添加原因

---
+++ back/checkerror.c
@@ -0,0 +1,9 @@
+/***********************************
+*
+*readme
+*ÓÃÓÚ¼ì²â´íÎó
+*
+***********************************/
+
+#include <errno.h>
+printf("errno value: %d, it means: %s",errno, strerror(errno));
